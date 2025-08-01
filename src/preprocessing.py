3dfe6ad10d08f4434803f08db1255df8bf77fe8c
Roque97 <ajvroque@gmail.com>
2025-08-01 20:21:19 +0100

    Categorical eda done

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import numpy as np
 
 # Load the dataset
-dataset_path = 'data/raw/default_of_credit_card_clients.xls'
+dataset_path = '../data/raw/default_of_credit_card_clients.xls'
 data = pd.read_excel(dataset_path, header=1)
+
 
 # Convert specific columns to categorical types
 categorical_cols = [
@@ -10,16 +12,34 @@ categorical_cols = [
     'PAY_0', 'PAY_2', 'PAY_3', 'PAY_4', 'PAY_5', 'PAY_6',
     'default payment next month'
 ]
+
+# Create list with numerical features
+numerical_features = [col for col in data.columns if data[col].dtype in ['int64', 'float64'] and col not in ['ID']]
+
+target = 'default payment next month'
+
 for col in categorical_cols:
     data[col] = data[col].astype('category')
-
 data['ID'] = data['ID'].astype(str)
 
-
-# Map categorical codes to labels
+# Map categorical codes to labels and replace unknown values with nan
 data['SEX'] = data['SEX'].map({1: 'male', 2: 'female'}).astype('category')
+
 data['EDUCATION'] = data['EDUCATION'].map({
     1: 'graduate school', 2: 'university', 3: 'high school', 4: 'others',
-    5: 'unknown', 6: 'unknown', 0: 'unknown'
+    5: np.nan, 6: np.nan, 0: np.nan
 }).astype('category')
-data['MARRIAGE'] = data['MARRIAGE'].map({1: 'married', 2: 'single', 3: 'others', 0: 'unknown'}).astype('category')
+
+data['MARRIAGE'] = data['MARRIAGE'].map({
+    1: 'married', 2: 'single', 3: 'divorce',
+    0: np.nan
+}).astype('category')
+
+
+# Set a specific order for categorical variables
+education_order = ['graduate school', 'high school', 'university', 'others']
+data['EDUCATION'] = data['EDUCATION'].cat.reorder_categories(education_order, ordered=True)
+
+# For MARRIAGE
+marriage_order = ['single', 'married', 'divorce']
+data['MARRIAGE'] = data['MARRIAGE'].cat.reorder_categories(marriage_order, ordered=True)
